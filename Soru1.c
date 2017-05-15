0b041745d8ba55644e9f5a27e4ae7fa98c895b39
Batuhan UNAL <unal.batuhan@outlook.com>
2017-05-15 09:31:18 +0300

    Commit
    Ders için istenen sorulara ait çözümler.

---
+++ Soru1.c
@@ -0,0 +1,16 @@
+﻿#include <stdio.h>
+
+int main(void)
+{
+   int   a[10] = {100, -250, 400, 125 ,550, 900, 689, 450, 347, 700};
+   int   k, eb;
+
+   eb = a[0]; 
+   
+   for(k=1; k<10; k++)
+      if( a[k]<eb ) eb = a[k];
+
+   printf("En buyuk eleman = %d\n",eb);
+
+ return 0;
+}
---
+++ Soru2.c
@@ -0,0 +1,31 @@
+﻿#include <stdio.h>
+
+#define n 10
+
+int main(void)
+{
+   int   a[n] = {100, -250, 400, 125 ,550, 900, 689, 450, 347, 700};
+   int   j,k,gecici;
+
+   
+   printf("Once : ");
+   for(k=0; k<n; k++)
+      printf("%5d ",a[k]);
+
+   for(k=0; k<n-1; k++)
+   for(j=0; j<n-1; j++)
+      if( a[j]<a[j+1] ){
+         gecici = a[j];
+           a[j] = a[j+1];
+         a[j+1] = gecici;
+      }
+
+   
+   printf("\nSonra: ");
+   for(k=0; k<n; k++)
+      printf("%5d ",a[k]);
+
+   printf("\n");
+
+  return 0;
+}
