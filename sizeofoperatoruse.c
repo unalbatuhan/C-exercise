5a55eadb94cd5f2b9b158501161965fb602455ba
Batuhan UNAL <unal.batuhan@outlook.com>
2017-05-21 22:04:52 +0300

    Read Commit
    C programı içerisinde, veri tiplerinin bellekte kapladığı alan sizeof operatörü ile öğrenilebilir. İlgi cekici olan, bu alanların derleyiciye ve işletim sistemine bağlı olarak değişiklik göstermesidir.sizeof operatörü kullanılarak, veri tiplerinin bellek uzunlularının nasıl ekrana yazdırılacağı gösterilmiştir.

---
+++ sizeofoperatoruse.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+main()
+{
+	printf("char : %d bayt\n", sizeof(char));
+	printf("short : %d bayt\n", sizeof(short));
+	printf("int : %d bayt\n", sizeof(int));
+	printf("long : %d bayt\n", sizeof(long));
+	printf("unsigned char : %d bayt\n", sizeof(unsigned char));
+	printf("unsigned short : %d bayt\n", sizeof(unsigned short));
+	printf("unsigned int : %d bayt\n", sizeof(unsigned int));
+	printf("unsigned long : %d bayt\n", sizeof(unsigned long));
+	printf("float : %d bayt\n", sizeof(float));
+	printf("double : %d bayt\n", sizeof(double));
+	printf("long double : %d bayt\n", sizeof(long double));
+}
