e69f0dce83901ebe880b50afb8bbccdd84ab8131
Batuhan UNAL <unal.batuhan@outlook.com>
2017-06-05 23:50:13 +0300

    Add files via upload

---
+++ En küçük bulan.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void)
+{
+   int   a[10] = {100, -250, 400, 125 ,550, 900, 689, 450, 347, 700};
+   int   k, eb;
+
+   /* ilk eleman en büyük kabul ediliyor */
+   eb = a[0]; 
+   
+   for(k=1; k<10; k++)
+      if( a[k]<eb ) eb = a[k];
+
+   printf("En buyuk eleman = %d\n",eb);
+
+ return 0;
+}
